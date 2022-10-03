7f9036258034ce4830523839508a70dcd4bc9b04
Dyllanus <74240279+Dyllanus@users.noreply.github.com>
2022-10-03 15:13:52 +0200

    nieuw project om requests te sturen

---
+++ MIC-RequestSender/RequestSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Nodes;
+
+namespace MIC_RequestSender;
+
+public class RequestSender
+{
+    public static JsonNode sendRequest()
+    {
+        return new JsonArray();
+    }
+}
